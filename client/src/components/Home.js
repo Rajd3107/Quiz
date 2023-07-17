bfae2acbac52fd81c79ad60aef18dfe5613940c0
Rajd3107 <85829896+Rajd3107@users.noreply.github.com>
2023-07-17 19:32:44 +0530

    Add files via upload
    Client and server added

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container mt-5 text-center text-light'>
+      <h1>SCOREHERO</h1>
+    </div>
+  )
+}
+
+export default Home
